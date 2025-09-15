31cbc67176398cb2a56fb3fe40012aa8f3f2f6f1
iagosantiago1 <iagosantiago@outlook.com>
2025-09-15 10:09:11 -0300

    feat: add main page with routing and material design components
    - Created a new Main component and added routing for '/main'.
    - Updated app component to include RouterOutlet.
    - Added Google Fonts and Material Icons in index.html.
    - Integrated Angular Material Sidenav in the Main component.
    - Created data series for the Main component.
    - Added basic styles for the Main component.
    - Implemented unit tests for the Main component.

---
+++ src/app/pages/main/main.ts
@@ -0,0 +1,16 @@
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+@Component({
+  selector: 'app-main',
+  imports: [
+    MatSidenavModule,
+  ],
+  templateUrl: './main.html',
+  styleUrl: './main.scss'
+})
+export class Main {
+
+  
+
+}
